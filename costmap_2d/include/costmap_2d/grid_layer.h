5b4742ccfcfea80e2bef4ed6062861de79d2c286
felderstehost <felderstehost@outlook.com>
2022-10-17 09:20:06 +0800

    add grid_layer

---
+++ costmap_2d/include/costmap_2d/grid_layer.h
@@ -0,0 +1,33 @@
+#ifndef GRID_LAYER_H_
+#define GRID_LAYER_H_
+#include <ros/ros.h>
+#include <costmap_2d/layer.h>
+#include <costmap_2d/layered_costmap.h>
+#include <costmap_2d/GenericPluginConfig.h>
+#include <dynamic_reconfigure/server.h>
+
+namespace costmap_2d
+{
+
+class GridLayer : public costmap_2d::Layer, public costmap_2d::Costmap2D
+{
+public:
+  GridLayer();
+
+  virtual void onInitialize();
+  virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y, double* max_x,
+                             double* max_y);
+  virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
+  bool isDiscretized()
+  {
+    return true;
+  }
+
+  virtual void matchSize();
+
+private:
+  void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
+  dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv_;
+};
+}
+#endif
